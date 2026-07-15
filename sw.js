3543f868a11c7bb69df4045d4042039a3b99478c
xoshawysarhad-png <xoshawysarhad@gmail.com>
2026-07-16 01:17:53 +0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = 'kstore-cache-v1';
+const assets = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/icon.png'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(assets);
+    })
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(response => {
+      return response || fetch(e.request);
+    })
+  );
+});
